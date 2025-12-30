2e27ba0b2ee4963f4026b78f47d7cd2210d9f163
Tina Wang <tinawang158@gmail.com>
2025-12-30 11:47:42 -0800

    Time: 247 ms (42.97%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-SELECT customer_number FROM Orders
-GROUP BY customer_number
-ORDER BY count(customer_number) desc
-LIMIT 1;
+select customer_number from Orders
+group by customer_number
+order by count(order_number) desc
+limit 1;
